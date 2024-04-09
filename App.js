a6d09463036b77feb9e2777e59042c2e93f14443
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-09 06:08:19 +0600

    Added skia

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// NPM MODULES
 import React from 'react';
 import {
   SafeAreaView,
@@ -8,13 +9,26 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {Canvas, Circle, Group} from '@shopify/react-native-skia';
 
+// UTILS
 import FONTS from './src/utils/fonts';
 
 function App() {
+  const width = 256;
+  const height = 256;
+  const r = width * 0.33;
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>I'm Anis</Text>
+      <Canvas style={{width, height}}>
+        <Group blendMode="multiply">
+          <Circle cx={r} cy={r} r={r} color="cyan" />
+          <Circle cx={width - r} cy={r} r={r} color="magenta" />
+          <Circle cx={width / 2} cy={width - r} r={r} color="yellow" />
+        </Group>
+      </Canvas>
     </SafeAreaView>
   );
 }
