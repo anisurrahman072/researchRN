0cd9669dd8ea7f3ba20c1ec4df9904d11533f254
anisurrahman072 <anisurrahmanbup@gmail.com>
2024-04-13 02:34:58 +0600

    Finally done screen transition

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+import 'react-native-gesture-handler'
+
 // NPM MODULES
 import React from 'react'
 import { StyleSheet } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createNativeStackNavigator } from 'react-native-screens/native-stack'
 import { GestureDetectorProvider } from 'react-native-screens/gesture-handler'
 
 // SCREENS
@@ -17,34 +19,21 @@ const Stack = createNativeStackNavigator()
 function App() {
 	return (
 		<GestureHandlerRootView style={styles.container}>
-			<NavigationContainer>
-				<GestureDetectorProvider>
-					<Stack.Navigator
-						screenOptions={{
-							stackAnimation: 'modal'
-						}}>
+			<GestureDetectorProvider>
+				<NavigationContainer>
+					<Stack.Navigator>
 						<Stack.Screen name="ScreenA" component={ScreenA} />
 						<Stack.Screen
 							name="ScreenB"
 							component={ScreenB}
 							options={{
-								title: 'ScreenB',
-								gestureEnabled: true,
-								goBackGesture: 'swipeDown' // gestures that trigger the screen transition
+								goBackGesture: 'twoDimensionalSwipe'
 							}}
 						/>
-						<Stack.Screen
-							name="ScreenC"
-							component={ScreenC}
-							options={{
-								title: 'ScreenC',
-								gestureEnabled: true,
-								goBackGesture: 'swipeUp' // gestures that trigger the screen transition
-							}}
-						/>
+						{/* <Stack.Screen name="ScreenC" component={ScreenC} /> */}
 					</Stack.Navigator>
-				</GestureDetectorProvider>
-			</NavigationContainer>
+				</NavigationContainer>
+			</GestureDetectorProvider>
 		</GestureHandlerRootView>
 	)
 }
